b39dda946e9c76b2c30262c55287c6c72371c022
Pablo Sanchez <pablosgrad12@gmail.com>
2022-01-26 18:59:24 -0700

    Added calcPay into overloaded constructor and defined constructors.

--- classesAndObjectsReview/employeePay.cpp
+++ classesAndObjectsReview/employeePay.cpp
@@ -5,3 +5,21 @@
 # include <vector>
 #include <fstream>
 using namespace std;
+
+//sets all variables to empty/0.
+employeePay::employeePay() {
+	this->firstName = " ";
+	this->lastName = " ";
+	this->ID = 0;
+	this->payRate = 0.0;
+	this->calcPay = 0.0;
+}
+
+//sets variables to values passed into constructor.
+employeePay::employeePay(string firstName, string lastName, int ID, double payRate, double calcPay) {
+	this->firstName = firstName;
+	this->lastName = lastName;
+	this->ID = ID;
+	this->payRate = payRate;
+	this->calcPay = calcPay;
+}
--- classesAndObjectsReview/employeePay.h
+++ classesAndObjectsReview/employeePay.h
@@ -1,15 +1,16 @@
 #pragma once
+# include <string>
 
 class employeePay
 {
 private:
 	//first and last name variables along 
 	//with ID number, a basepay that will be 
-	//15,000 for each employee and the payrate that
-	//will be assigned along with calculated pay.
+	//15,000 for each employee, the payrate that
+	//will be assigned and calculated pay.
 	string firstName, lastName;
 	int ID;
-	const int basepay = 15000;
+	const int basePay = 15000;
 	double payRate;
 	double calcPay;
 
@@ -17,8 +18,9 @@ public:
 	//default constructor
 	employeePay();
 
-	//overloaded constructor with all but basePay and calcPay as parameters
-	employeePay(string firstName, string lastName, int ID, double payRate);
+	//overloaded constructor with all variables
+	//except basePay as parameters
+	employeePay(string firstName, string lastName, int ID, double payRate, double calcPay);
 	
 };
 
